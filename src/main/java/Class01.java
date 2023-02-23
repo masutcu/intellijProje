a5ec233f48d698a4109f4248640daecbd17aba83
masutcu <masutcu@gmail.com>
2023-02-24 00:03:59 +0300

    v2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,5 +1,7 @@
 public class Class01 {
     public static void main(String[] args) {
         System.out.println("first study -birinci calısmam");
+        System.out.println("v3");
+        System.out.println("bugün hava yağışlı");
     }
 }
